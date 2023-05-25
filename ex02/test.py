fb97a00b01efb2f975a261d584ef13aea1b0d1dc
Augustin Debureaux <adebureaux@gmail.com>
2023-05-25 18:03:37 +0200

    vector work in porgress almost finished, dot and T + testing

--- ex02/test.py
+++ ex02/test.py
@@ -1,43 +1,85 @@
 from vector import Vector
 
-# Column vector of shape (n, 1)
-v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-print(v1.shape)
-print(v1.values)
+if __name__ == "__main__":
+	def test_should_fail(vec):
+		try:
+			Vector(vec)
+			return False
+		except (ValueError, TypeError, NotImplementedError) as e:
+			print(e)
+		return True
 
-# Row vector of shape (1, n)
-v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
-print(v2.shape)
-print(v2.values)
+	# Column vector of shape (n, 1)
+	v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+	print(v1.shape)
+	print(v1.values)
 
-# Vector initialized with a size
-v3 = Vector(3)
-print(v3)
+	# Row vector of shape (1, n)
+	v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
+	print(v2.shape)
+	print(v2.values)
 
-# Vector initialized with a range
-v4 = Vector((16, 18))
-print(v4)
+	# Column vector init with integer
+	print(Vector([[1], [2]]))
 
-# Vector initialized with a range
-v5 = Vector([[9.], [0.]])
-print(v5)
+	# Row vector init with integer
+	print(Vector([[1, 2]]))
 
-try:
-	Vector('abc')
-except (ValueError, TypeError) as e:
-	print(e)
+	# Vector initialized with a size
+	v3 = Vector(2)
+	print(v3)
+	print(v3.shape)
 
-try:
-	Vector([1])
-except (ValueError, TypeError) as e:
-	print(e)
+	# Vector initialized with a range
+	v4 = Vector((16, 17))
+	print(v4)
+	print(v4.shape)
 
-try:
-	Vector([[1, 2]])
-except (ValueError, TypeError) as e:
-	print('1', e)
+	# Vector initialized with a negative range
+	v5 = Vector((-5, 1))
+	print(v5)
 
-try:
-	Vector([[1], [2]])
-except (ValueError, TypeError) as e:
-	print(e)
+	# Vector-Vector addition
+	print(v3 + v3)
+
+	# Vector-Scalar addition
+	print(v3 + 8)
+
+	# Scalar-Vector addition
+	print(5 + v3)
+
+	# Vector-Vector substraction
+	print(v3 - v3)
+
+	# Vector-Scalar substraction
+	print(v3 - 3)
+
+	# Scalar-Vector substraction
+	print(5 - v3)
+
+	# Vector-Scalar division
+	print(v4 / 1.52)
+
+	# Vector-Scalar multiplication
+	print(v4 * 2)
+
+	# Vector-Scalar multiplication
+	print(3 * v2)
+
+	if not test_should_fail('abc'):
+		print('Failed')
+
+	if not test_should_fail([1]):
+		print('Failed')
+
+	if not test_should_fail([['a'], ['b']]):
+		print('Failed')
+
+	if not test_should_fail([[1.], [1., 2., 3.], [1.]]):
+		print('Failed')
+
+	if not test_should_fail(0):
+		print('Failed')
+
+	if not test_should_fail((16, 10)):
+		print('Failed')
--- ex02/vector.py
+++ ex02/vector.py
@@ -3,31 +3,81 @@ class Vector:
 
 		# Initialization by size
 		if isinstance(values, int):
+			if values <= 0:
+				raise ValueError("__init__ invalid size: value must be positive.")
 			self.values = [[float(i)] for i in range(values)]
 
 		# Initialization by range
 		elif isinstance(values, tuple):
 			start, end = values
 			if start > end:
-				raise ValueError("__init__ tuple invalid range: start value must be smaller than end value.")
+				raise ValueError("__init__ invalid range: start value must be smaller or equal than end value.")
 			self.values = [[float(i)] for i in range(start, end + 1)]
 
 		# Initialization by list of a list of floats or list of lists of single float
 		elif isinstance(values, list):
-			if all(isinstance(sublist, list) and len(sublist) == 1 for sublist in values):
+			if all(isinstance(sublist, list) and all(isinstance(val, (int, float)) for val in sublist) and len(sublist) == 1 for sublist in values):
 				self.values = [[float(val) for val in sublist] for sublist in values]
-			elif all(isinstance(sublist, list) and all(isinstance(val, float) for val in sublist) for sublist in values):
-				self.values = values
+			elif all(isinstance(sublist, list) and all(isinstance(val, (int, float)) for val in sublist) and len(sublist) != 1 for sublist in values):
+				self.values = [[float(val) for val in sublist] for sublist in values]
 			else:
-				raise ValueError("__init__ invalid parameter")
+				raise ValueError("__init__ invalid parameter.")
 
 		# Initialization error
-		if self.values:
-			raise ValueError("__init__ invalid parameter")
+		else:
+			raise TypeError("__init__ invalid parameter.")
 
+		# Initialization of shape
 		self.shape = (len(self.values), 1) if len(self.values) > 1 else (1, len(self.values[0]))
 
-	def __str__(self):
-		return str(f'Vector({self.values})')
+	# Addition operator
+	def __add__(self, other):
+		if isinstance(other, (int, float)):
+			result = [[x + other for x in val] for val in self.values]
+			return Vector(result)
+		if self.shape != other.shape:
+			raise ValueError("__add__ vector shapes do not match.")
+		result = [[x + y for x, y in zip(v1, v2)] for v1, v2 in zip(self.values, other.values)]
+		return Vector(result)
 
-	
+	def __radd__(self, other):
+		return self.__add__(other)
+
+	# Substraction operator
+	def __sub__(self, other):
+		if isinstance(other, (int, float)):
+			result = [[x - other for x in val] for val in self.values]
+			return Vector(result)
+		if self.shape != other.shape:
+			raise ValueError("__sub__ vector shapes do not match.")
+		result = [[x - y for x, y in zip(v1, v2)] for v1, v2 in zip(self.values, other.values)]
+		return Vector(result)
+
+	def __rsub__(self, other):
+		return self.__sub__(other)
+
+	# Division operator
+	def __truediv__(self, other):
+		if not isinstance(other, (int, float)):
+			raise TypeError("__truediv__ only works with scalars that in either an integer or a float.")
+		result = [[x / other for x in val] for val in self.values]
+		return Vector(result)
+
+	def __rtruediv__(self, other):
+		raise NotImplementedError("__rtruediv__ division of a scalar by a Vector is not defined here.")
+
+	# Multiplication operator
+	def __mul__(self, other):
+		if not isinstance(other, (int, float)):
+			raise TypeError("__mul__ only works with scalars that in either an integer or a float.")
+		result = [[x * other for x in val] for val in self.values]
+		return Vector(result)
+
+	def __rmul__(self, other):
+		return self.__mul__(other)
+
+	def __str__(self):
+		return str(self.values)
+
+	def __repr__(self):
+		return self.__str__()
