d4a89dd1db46764911a80ce887744859bd6cddb6
Augustin Debureaux <adebureaux@gmail.com>
2023-05-26 15:09:11 +0200

    Init ex03

---
+++ ex03/generator.py
@@ -0,0 +1,4 @@
+def generator(text, sep=" ", option=None):
+'''Splits the text according to sep value and yield the substrings.
+option precise if a action is performed to the substrings before it is yielded.
+'''
