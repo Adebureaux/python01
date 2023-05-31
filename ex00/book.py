c9ff49f58af8333d697390e4d86b538491cb2c89
Augustin Debureaux <adebureaux@gmail.com>
2023-05-31 13:19:36 +0200

    Fix ex00

--- ex00/book.py
+++ ex00/book.py
@@ -4,43 +4,42 @@ from recipe import Recipe
 
 class Book:
 	
-	def __init__(self, name, last_update, creation_date, recipes_list):
+	def __init__(self, name, recipes_list={}):
 		if not isinstance(name, str) or not len(name):
 			raise ValueError('Book __init__ Book name must be a string that contains at least one character.')
 		self.name = name
 
-		if not isinstance(last_update, datetime):
-			raise TypeError('Book __init__ Book last_update must be a datetime object.')
-		self.last_update = last_update
-
-		if not isinstance(creation_date, datetime):
-			raise TypeError('Book __init__ Book creation_date must be a datetime object.')
-		self.creation_date = creation_date
-
-		if not isinstance(recipes_list, dict) or not all(key in ['starter', 'lunch', 'dessert'] for key in recipes_list.keys()):
-			raise ValueError("Book __init__ recipes_list must be a dictionary with keys that are either 'starter', 'lunch', or 'dessert'")
-		if not all(isinstance(value, list) and all(isinstance(recipe, Recipe) for recipe in value) for value in recipes_list.values()):
-			raise TypeError('Book __init__ every value in recipes_list must be a list of instances of Recipe')
+		if not isinstance(recipes_list, dict):
+			raise TypeError('__init__ recipes_list must be a dictionary.')
+		if len(recipes_list):
+			if not all(key in ['starter', 'lunch', 'dessert'] for key in recipes_list.keys()):
+				raise ValueError("Book __init__ recipes_list keys must be either 'starter', 'lunch', or 'dessert'.")
+			if not all(isinstance(value, list) and all(isinstance(recipe, Recipe) for recipe in value) for value in recipes_list.values()):
+				raise TypeError('Book __init__ every value in recipes_list must be a list of instances of Recipe.')
 		self.recipes_list = recipes_list
+		self.creation_date = datetime.today()
+		self.last_update = self.creation_date
 
 	def get_recipe_by_name(self, name):
 		'''Prints a recipe with the name \texttt{name} and returns the instance'''
 		for recipes in self.recipes_list.values():
 				for recipe in recipes:
 						if recipe.name == name:
+								print(recipe)
 								return recipe
+		print("Recipe not found.")
 		return None
 
 	def get_recipes_by_types(self, recipe_type):
 		'''Get all recipe names for a given recipe_type'''
 		if not recipe_type in ['starter', 'lunch', 'dessert']:
-			raise ValueError("Book get_recipes_by_types() argument recipe_type must be a string which can be 'starter', 'lunch' or 'dessert'")
+			raise ValueError("Book get_recipes_by_types() argument recipe_type must be a string which can be 'starter', 'lunch' or 'dessert'.")
 		return [recipe.name for recipes in self.recipes_list.values() for recipe in recipes if recipe.recipe_type == recipe_type]
 
 	def add_recipe(self, recipe):
 		'''Add a recipe to the book and update last_update'''
 		if not isinstance(recipe, Recipe):
-			raise TypeError('Book add_recipe() argument recipe must be a Recipe instance')
+			raise TypeError('Book add_recipe() argument recipe must be a Recipe instance.')
 		self.last_update = datetime.today()
 		recipe_list = self.recipes_list.get(recipe.recipe_type, [])
 		recipe_list.append(recipe)
--- ex00/test.py
+++ ex00/test.py
@@ -7,18 +7,17 @@ from time import sleep
 # Tests that should not raise an exception:
 try:
 	# Create some recipes
-	pancakes = Recipe("Pancakes", 2, 20, ['flour', 'milk', 'eggs'], 'starter', "Classic fluffy pancakes")
-	bolognese = Recipe("Spaghetti Bolognese", 3, 45, ['spaghetti', 'ground beef', 'tomatoes'], 'lunch', "Hearty Italian pasta dish")
-	cookies = Recipe("Chocolate Chip Cookies", 2, 15, ['flour', 'butter', 'chocolate chips'], 'dessert', "Homemade sweet treats")
-	chicken = Recipe("Chicken Stir-Fry", 4, 25, ['chicken breast', 'vegetables', 'soy sauce'], 'lunch', "Quick and flavorful Asian dish")
-	smoothie = Recipe("Berry Smoothie", 1, 5, ['berries', 'yogurt', 'honey'], 'dessert', "Refreshing and nutritious drink")
+	pancakes = Recipe("Pancakes", 2, 20, ['flour', 'milk', 'eggs'], "Classic fluffy pancakes", 'starter')
+	bolognese = Recipe("Spaghetti Bolognese", 3, 45, ['spaghetti', 'ground beef', 'tomatoes'], "Hearty Italian pasta dish", 'lunch')
+	cookies = Recipe("Chocolate Chip Cookies", 2, 15, ['flour', 'butter', 'chocolate chips'], "Homemade sweet treats", 'dessert')
+	chicken = Recipe("Chicken Stir-Fry", 4, 25, ['chicken breast', 'vegetables', 'soy sauce'], "Quick and flavorful Asian dish", 'lunch')
+	smoothie = Recipe("Berry Smoothie", 1, 5, ['berries', 'yogurt', 'honey'], "Refreshing and nutritious drink", 'dessert')
 
 	# Create a book and add recipes
-	book = Book("Recettes", datetime.today(), datetime.today(), {'starter': [pancakes], 'lunch': [bolognese, chicken], 'dessert': [cookies, smoothie]})
+	book = Book("Recettes", {'starter': [pancakes], 'lunch': [bolognese, chicken], 'dessert': [cookies, smoothie]})
 
 	# Test get_recipe_by_name
 	recipe = book.get_recipe_by_name("Chicken Stir-Fry")
-	print(recipe)  # Should print the recipe details
 
 	recipe = book.get_recipe_by_name("Salad")  # Recipe not found
 	print(recipe)  # Should print None
@@ -31,7 +30,7 @@ try:
 	print(recipes)  # Should print a list of lunch recipe names
 
 	# Test add_recipe
-	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], 'starter', "Fresh and healthy salad")
+	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], "Fresh and healthy salad", 'starter')
 	book.add_recipe(new_recipe)
 
 	recipes = book.get_recipes_by_types("starter")
@@ -39,7 +38,7 @@ try:
 
 	# Test last_update
 	before_update = book.last_update
-	new_recipe = Recipe("Pasta", 1, 20, ['pasta', 'sauce'], 'lunch', "Sample description")
+	new_recipe = Recipe("Pasta", 1, 20, ['pasta', 'sauce'], "Sample description", 'lunch')
 	book.add_recipe(new_recipe)
 	print(str(book))
 	if before_update == book.last_update:  # before_update should not be equal last_update
@@ -56,41 +55,41 @@ except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], 'breakfast', "Fresh and healthy salad")  # Invalid recipe type
+	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], "Fresh and healthy salad", 'breakfast')  # Invalid recipe type
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], 5, "Fresh and healthy salad")  # Invalid recipe type
+	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], "Fresh and healthy salad", 5)  # Invalid recipe type
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], 'starter', 5)  # Invalid description
+	new_recipe = Recipe("Salad", 1, 10, ['lettuce', 'tomatoes', 'cucumber'], 5, 'starter')  # Invalid description
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	new_recipe = Recipe("Pasta", -1, 20, ['pasta', 'sauce'], 'lunch', "Sample description")  # Negative cooking time
+	new_recipe = Recipe("Pasta", -1, 20, ['pasta', 'sauce'], "Sample description", 'lunch')  # Negative cooking time
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	new_recipe = Recipe("Soup", 3, 25, 'ingredient', 'starter', "Sample description")  # Invalid ingredients (not a list)
+	new_recipe = Recipe("Soup", 3, 25, 'ingredient', "Sample description", 'starter')  # Invalid ingredients (not a list)
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	new_recipe = Recipe("Salad", 2, 15, ['lettuce', 'tomatoes', 5], 'lunch', "Sample description")  # Invalid ingredient (not a string)
+	new_recipe = Recipe("Salad", 2, 15, ['lettuce', 'tomatoes', 5], "Sample description", 'lunch')  # Invalid ingredient (not a string)
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	book = Book("Recettes", datetime.today(), datetime.today(), {'starter': pancakes})  # Invalid recipe_list dict value (not a list)
+	book = Book("Recettes", {'starter': pancakes})  # Invalid recipe_list dict value (not a list)
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
 
 try:
-	book = Book("Recettes", datetime.today(), datetime.today(), ['starter', pancakes])  # Invalid recipe_list (not a dict)
+	book = Book("Recettes", ['starter', pancakes])  # Invalid recipe_list (not a dict)
 except (ValueError, TypeError) as e:
 	print(f'Invalid parameter: {e.args[0]}', file=stderr)
